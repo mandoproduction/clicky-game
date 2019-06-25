ef5ace163dafcb71e584e4eface56b43ae4ee999
Craig <craigdwyer73@gmail.com>
2019-06-25 18:00:49 -0400

    Updates

---
+++ precache-manifest.4ea4fb8984c8e21c34acd35ca3542991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6782d8fc545d56968a009fb4151d6c6c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "517c94531464fa37124b",
+    "url": "/clicky-game/static/css/main.40a8110d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "517c94531464fa37124b",
+    "url": "/clicky-game/static/js/main.42f65d61.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
